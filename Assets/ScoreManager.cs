7d0be63a40dd0eef629819826c6207b674dbbf54
Enze Mao <Enze@Enzes-Mac-mini.local>
2017-05-13 03:05:43 +0100

    simple score system

---
+++ Assets/ScoreManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+	public static int score = 0;
+	public Text scoreText;
+
+	[SerializeField] private float addScoreTime = 1f;
+	[SerializeField] private float addScoreCounter;
+	// Use this for initialization
+	void Start () {
+		addScoreCounter = addScoreTime;	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (!Player.isDead) {
+			addScoreCounter -= Time.deltaTime;
+			if (addScoreCounter < 0) {
+				score++;
+				scoreText.text = score.ToString();
+				addScoreCounter = addScoreTime;
+			}
+		}
+	}
+}

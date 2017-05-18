53eb61b1284d7fef4d90bbd6f7d3ec08e36c6fce
Enze Mao <Enze@Enzes-Mac-mini.local>
2017-05-18 11:33:46 +0100

    add best score on screen

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -6,8 +6,10 @@ using UnityEngine.UI;
 public class ScoreManager : MonoBehaviour {
 
 	public static int score;
+	public static int bestScore = 0;
 	public Text scoreText;
 	public Text speedText;
+	public Text bestScoreText;
 	public Player player;
 
 	[SerializeField] private float addScoreTime = 1f;
@@ -30,6 +32,12 @@ public class ScoreManager : MonoBehaviour {
 			}
 			//monitor player speed
 			speedText.text = player.m_moveSpeed.ToString ();
+			//update best score when player had dead
+		} else if (Player.isDead) {
+			if (score > bestScore) {
+				bestScore = score;
+			}
 		}
+		bestScoreText.text = bestScore.ToString ();
 	}
 }
